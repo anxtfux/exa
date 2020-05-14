a086e37c1caa48c1357e03b178c790e11191743a
Amer Banet <anxtfux@gmail.com>
2020-05-14 22:44:52 +0200

    Show hidden files in Grey and with a different icon.

--- src/output/icons.rs
+++ src/output/icons.rs
@@ -44,6 +44,7 @@ pub fn painted_icon(file: &File, style: &FileStyle) -> String {
 
 fn icon(file: &File) -> char {
     if file.is_directory() { '\u{f115}' }
+    else if file.is_hidden() { '\u{fb12}' }
     else if let Some(icon) = Box::new(FileExtensions).icon_file(file) { icon }
     else if let Some(ext) = file.ext.as_ref() {
         match ext.as_str() {
